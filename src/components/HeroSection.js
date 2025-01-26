53e32be84c1187fd34b9405b6d3b5d1da441c8ee
MariaCamilaHincapieLuna <macahlu@gmail.com>
2025-01-26 17:26:51 -0500

    fix media problem

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ export default function HeroSection() {
   return (
     <div className='hero-container'>
       {/* Video section */}
-        <video src='/videos/video-1.mp4' autoPlay loop muted></video>
+        <video src={`${process.env.PUBLIC_URL}/videos/video-1.mp4`} autoPlay loop muted></video>
         <h1>ADVENTURE AWAITS</h1>
         <p>What are you waiting for?</p>
         <div className='hero-btns'>
